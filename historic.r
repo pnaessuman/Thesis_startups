f4e88097774959d9414813eda544cbf3acfb36bc
Nana Amponsah Essuman <nana@Nanas-MacBook-Pro.fritz.box>
2025-10-01 12:59:53 +0200

    Code buildup

--- historic.r
+++ historic.r
@@ -3,13 +3,14 @@ library(treeclim)
 library(pointRes)
 
 
-beech <- read.rwl("Data/buche_chrono.rwl")
-climate_beech <- read.csv2("Data/climate_bausenberg.csv")[, c(1, 2, 3, 6)]
-spruce <- read.rwl("Data/spruce_alpine.rwl")
-climate_spruce <- read.csv2("Data/climate_alpine.csv")
-
+beech <- read.rwl("../Data/buche_chrono.rwl")
+climate_beech <- read.csv2("../Data/climate_bausenberg.csv")[, c(1, 2, 3, 6)]
+spruce <- read.rwl("../Data/spruce_alpine.rwl")
+climate_spruce <- read.csv2("../Data/climate_alpine.csv")
+source("../External/Vs.R")
 
 #Chron
+
 beech_c <- chron(beech)
 spruce_c <- chron(spruce)
 
@@ -18,8 +19,10 @@ plot(beech_c)
 #filter drought years(1976 and 2003) for both beech and spruce
 
 climate_beech$year <- as.numeric(climate_beech$year)  # Ensure year is numeric
-climate_years_b <- climate_data[climate_beech$year %in% c(1976, 2003), ]
+climate_years_b <- climate_beech[climate_beech$year %in% c(1976, 2003), ]
 
-climate_spruce$year <- as.numeric(climate_spruce$year)  # Ensure year is numeric
-climate_years_s <- climate_data[climate_spruce$year %in% c(1976, 2003), ]
+climate_spruce$year <- as.numeric(climate_spruce$year)  
+climate_years_s <- climate_spruce[climate_spruce$year %in% c(1976, 2003), ]
+
+
 
