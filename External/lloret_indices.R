54cb06aba1ce2065f0882c79c612f9e23388dab3
Nana Amponsah Essuman <nana@MacBookPro.fritz.box>
2025-10-11 17:05:12 +0200

    added new

--- External/lloret_indices.R
+++ External/lloret_indices.R
@@ -19,14 +19,41 @@ res_postdr <- function(x, years, n = 3) {
   })
 }
 
+# x: data.frame with dendro data
+# y: vector of critical years
+# n: number of years before and after the critical
+#    years
 resistance <- function(x, years, n = 3) {
-  res_dr(x, years) / res_predr(x, years, n)
+  out <- res_dr(x, years) / res_predr(x, years, n)
+  data.frame(
+    year = years,
+    index = out
+  )
 }
 
 recovery <- function(x, years, n = 3) {
-  res_postdr(x, years, n) / res_dr(x, years)
+  out <- res_postdr(x, years, n) / res_dr(x, years)
+  data.frame(
+    year = years,
+    index = out
+  )
 }
 
 resilience <- function(x, years, n = 3) {
-  res_postdr(x, years, n) / res_predr(x, years, n)
+  out <- res_postdr(x, years, n) / res_predr(x, years, n)
+  data.frame(
+    year = years,
+    index = out
+  )
 }
+
+# USAGE EXAMPLE
+
+library(dplR)
+
+beech <- read.rwl("data/buche_chrono.rwl")
+
+drought_years <- c(1976, 2003)
+
+resistance(beech, drought_years)
+
